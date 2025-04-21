efa46672d67dd6a40f5e6d230a1982656eaa9f3f
yamahaboy <aleksej.ivanov2003@gmail.com>
2025-04-21 09:40:34 +0300

    Fix Dockerfile and start.sh for Render

--- src/HttpServerGame.java
+++ src/HttpServerGame.java
@@ -11,7 +11,7 @@ public class HttpServerGame {
     public static void main(String[] args) throws IOException {
         int port = Integer.parseInt(System.getenv("PORT"));
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", port), 0);
         server.createContext("/", new RootHandler());
         server.setExecutor(null);
         server.start();
